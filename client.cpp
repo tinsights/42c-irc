8d07cce5959f3b36d23298d3fedc8143084cf0ab
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-10-01 16:48:19 +0800

    add comments to system calls

--- client.cpp
+++ client.cpp
@@ -21,10 +21,15 @@
 
 using std::cout;
 using std::endl;
+using std::string;
 
-int main (void) {
-	cout << "what am i doing?" << endl;
+int main (int ac, char **av) {
+	string msg = "";
 
+	if (ac == 2)
+		msg = av[1];
+	
+	cout << msg << endl;
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	sockaddr_in server_addr = {
 		.sin_family = AF_INET,
@@ -34,16 +39,21 @@ int main (void) {
 
 	bind(server_socket,reinterpret_cast<struct sockaddr*>(&server_addr), sizeof server_addr);
 	connect(server_socket,reinterpret_cast<struct sockaddr*>(&server_addr), sizeof server_addr);
-	std::string buffer;
 
-	ssize_t sz;
-	
-	do {
-		std::getline(std::cin, buffer);
-		sz = send(server_socket, buffer.c_str(), buffer.length(), 0);
-		cout << "sz: " << sz << endl;
-	} while (sz > 0);
-	std::perror("error");
-	cout << server_socket << endl;
+	if (ac == 2)
+		send(server_socket, msg.c_str(), msg.length(), 0);
+	else {
+		std::string buffer;
+
+		ssize_t sz;
+		
+		do {
+			std::getline(std::cin, buffer);
+			sz = send(server_socket, buffer.c_str(), buffer.length(), 0);
+			cout << "sz: " << sz << endl;
+		} while (sz > 0);
+		std::perror("error");
+		cout << server_socket << endl;
+	}
 	close(server_socket);
 }
--- main.cpp
+++ main.cpp
@@ -15,22 +15,85 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
+
 using std::cout;
 using std::endl;
 
+typedef struct addrinfo ai;
+
 int main(void) {
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	/**
+	 * //OLD WAY! :
+	 * sockaddr_in server_addr = {
+	 * 	.sin_family = AF_INET,		// Address Family IPV4
+	 * 	.sin_port = htons(8080),	// Host to network byte order (short) 8080
+	 * 	.sin_addr = { INADDR_ANY }, // Accept from any address
+	 * };
+	 * 
+	 * new way of getting config options:
+	*/
+	ai hints, *res;
+
+	memset(&hints, 0, sizeof hints);
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, "8080", &hints, &res);
 	
-	sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET; // ipv4 protocol
-	server_addr.sin_port = htons(8080); // host to network byte order (short)
-	server_addr.sin_addr.s_addr = INADDR_ANY; // any address is allowed to connect
+	/**
+	 * #include <sys/types.h>
+	 * #include <sys/socket.h>
+	 * int socket(int domain, int type, int protocol);
+	 * 
+	 * socket() returns a new file descriptor configured to act as a socket
+	 * with the specified domain (Protocol Family), type (datagram or TCP),
+	 * and protocol (getprotobyname())
+	 * 
+	 * TODO: error checking (i.e. check return value)
+	*/
+	int server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-	bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr));
+	/**
+	 * int bind(int sockfd, struct sockaddr *my_addr, socklen_t addrlen);
+	 * 
+	 * bind() associates a socket with an IP address and a port number.
+	 * Prior to this a socket is just a file / file descriptor.
+	 * this socket listens to connections.
+	 * 
+	 * TODO: error checking (i.e. check return value)
+	*/
+	bind(server_socket, res->ai_addr, res->ai_addrlen);
 
-	listen(server_socket, 5);
+	/**
+	 * int listen(int s, int backlog);
+	 * 
+	 * This instructs the socket to listen for incoming connections from client programs:
+	 * The second argument is the number of incoming connections
+	 * that can be queued before you call accept(), below.
+	 * 
+	 * If there are this many connections waiting to be accepted,
+	 * additional clients will generate the error ECONNREFUSED.
+	 * 
+	 * TODO: error checking (i.e. check return value)
+	*/
+	listen(server_socket, 10);
 
-	int client_socket = accept(server_socket, NULL, NULL);
+	/**
+	 * int accept(int s, struct sockaddr *addr, socklen_t *addrlen)
+	 * 
+	 * accept() returns another socket file descriptor, one that is connected to the client.
+	 * it fills sockaddr *addr with information about the connecting client, 
+	 * and returns a new socket FD that can be used for send() and recv()
+	 * 
+	 * TODO: error checking (i.e. check return value)
+	*/
+	sockaddr_storage remote;
+	socklen_t len = sizeof remote;
+	int client_socket = accept(server_socket, reinterpret_cast<sockaddr *>(&remote), &len);
+
 
 	char buffer[1024] = {0};
 	int sz = 0;
@@ -43,5 +106,4 @@ int main(void) {
 	cout << client_socket << endl;
 	close(server_socket);
 	close(client_socket);
-	
 }
